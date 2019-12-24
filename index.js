b630c0ec2da27b06f68ce758c8578e69ca5501a4
Ethan <58045794+TheCrazyChicken@users.noreply.github.com>
2019-12-24 20:20:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const fs = require("fs")
 const Discord = require("discord.js");
 const Client = require("./client/Client");
-const { 
-    prefix, 
-    token, 
-} = require("./config.json");
+const { prefix, token } = require("./config.json");
 
 const client = new Client();
 client.commands = new Discord.Collection();
